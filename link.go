096dcc2f79fac65f1d10ef16e3b552122377e740
Arman Elahi <arasela95@gmail.com>
2018-05-14 22:42:34 -0700

    Added unstructured logging using logrus
    TODO: Add better, structured logging for an application log

--- link.go
+++ link.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/gomodule/redigo/redis"
+	log "github.com/sirupsen/logrus"
 )
 
 // Link represents a Link in the system
@@ -34,44 +35,52 @@ func (l *Link) Commit(c redis.Conn) bool {
 		"visits", l.Visits)
 
 	if err != nil {
-		fmt.Printf("REDIS ERROR: %#v\n", err)
+		log.Error(fmt.Sprintf("95670::REDIS ERROR: %e", err))
 		return false
 	}
 
 	l.DateModified = now
-	fmt.Printf("OK")
+	log.Info(fmt.Sprintf("95671::Committed link %#v", l))
 	return true
 }
 
 // Visit increments the counter for the number of visits
 func (l *Link) Visit(c redis.Conn) bool {
+	log.Debug(fmt.Sprintf("14230::Visiting link %s", l.Title))
 	key := l.key()
-
+	log.Debug(fmt.Sprintf("14231::Generated key %s", key))
 	visits, err := c.Do("HINCRBY", key, "visits", 1)
 
 	if err != nil {
-		fmt.Printf("REDIS ERROR: %#v\n", err)
+		log.Error(fmt.Sprintf("14232::REDIS ERROR %#v", err))
 		return false
 	}
 
-	fmt.Printf("OK %s", visits)
+	l.Visits, err = redis.Int64(visits, nil)
+	if err != nil {
+		log.Error(fmt.Sprintf("14234::Couldn't serialize visits to an int64"))
+		return false
+	}
+	log.Debug(fmt.Sprintf("14233::Incremented number of visits: %d", l.Visits))
+
 	return true
 }
 
 // GetLink obtains a Link hash from Redis
 func GetLink(c redis.Conn, scope string, user string, title string) (Link, error) {
+	log.Info(fmt.Sprintf("41230::Sending HGETALL to Redis for link:%s:%s:%s", scope, user, title))
 	hash, err := redis.Values(c.Do("HGETALL", fmt.Sprintf("link:%s:%s:%s", scope, user, title)))
 
 	if err != nil {
-		fmt.Printf("REDIS ERROR: %#v\n", err)
+		log.Error(fmt.Sprintf("41231::REDIS ERROR: %e", err))
 		return Link{}, err
 	}
 
 	link := HashToLink(hash)
-
 	link.User = user
 	link.Scope = scope
 	link.Title = title
+	log.Debug(fmt.Sprintf("41233::Filled in user, scope, title values"))
 
 	return link, nil
 }
@@ -85,31 +94,43 @@ func HashToLink(v []interface{}) Link {
 	var err error
 
 	for i := 0; i < len(v)-1; i = i + 2 {
-		key := v[i]
+		key, err := redis.String(v[i], err)
+		if err != nil {
+			log.Error(fmt.Sprintf("48716::Could not convert key to String %e", err))
+			continue
+		}
 		value := v[i+1]
+		log.Debug(fmt.Sprintf("48711::Trying to correlate %s %s", key, value))
 		if key == "link" {
-			link, err = redis.String(value, nil)
+			link, err = redis.String(value, err)
 			if err != nil {
+				log.Error(fmt.Sprintf("48712::Couldn't convert link to string: %e", err))
 				link = ""
 			}
 		} else if key == "date_modified" {
-			dateModified, err = redis.Int64(value, nil)
+			dateModified, err = redis.Int64(value, err)
 			if err != nil {
+				log.Error(fmt.Sprintf("48713::Couldn't convert date_modified to int64: %e", err))
 				dateModified = -1
 			}
 		} else if key == "date_created" {
-			dateCreated, err = redis.Int64(value, nil)
+			dateCreated, err = redis.Int64(value, err)
 			if err != nil {
+				log.Error(fmt.Sprintf("48714::Couldn't convert date_created to int64: %e", err))
 				dateCreated = -1
 			}
 		} else if key == "visits" {
-			visits, err = redis.Int64(value, nil)
+			visits, err = redis.Int64(value, err)
 			if err != nil {
+				log.Error(fmt.Sprintf("48713::Couldn't convert date_modified to int64: %e", err))
 				visits = -1
 			}
+		} else {
+			log.Warn("48715::Could not correlate last key value pair")
 		}
 	}
 
+	log.Info(fmt.Sprintf("48714::Returning Link {link:%s, dateModified:%d, dateCreated:%d, visits:%d}", link, dateModified, dateCreated, visits))
 	return Link{
 		User:         "",
 		Link:         link,
--- server/main.go
+++ server/main.go
@@ -8,18 +8,21 @@ import (
 
 	"github.com/gomodule/redigo/redis"
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/elahi-arman/urlshortener"
 )
 
 // PostLinkHandler retrieves data about the given link
 func PostLinkHandler(w http.ResponseWriter, r *http.Request) {
-	address := fmt.Sprintf("%s:%s", "127.0.0.1", "6379")
-	decoder := json.NewDecoder(r.Body)
-
 	var l urlshortener.Link
+
+	address := fmt.Sprintf("%s:%s", "127.0.0.1", "6379")
+	log.Debug("74530::Received a POST, decoding body")
+	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&l)
 	if err != nil {
+		log.Error(fmt.Sprintf("74531::Could not decode post body %e", err))
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -28,9 +31,10 @@ func PostLinkHandler(w http.ResponseWriter, r *http.Request) {
 	l.Scope = "global"
 	l.User = "arman"
 
+	log.Debug("74532::Connecting to Redis")
 	c, err := redis.Dial("tcp", address)
 	if err != nil {
-		fmt.Printf("Redis Dial Error %e", err)
+		log.Error(fmt.Sprintf("74533::Could not connect to redis %e", err))
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 	defer c.Close()
@@ -39,11 +43,11 @@ func PostLinkHandler(w http.ResponseWriter, r *http.Request) {
 	l.DateModified = time.Now().Unix()
 	l.Visits = 0
 
-	fmt.Printf("%d %+v\n", time.Now().Unix(), l)
-
 	if l.Commit(c) {
+		log.Info("74534::Committed link 201")
 		w.WriteHeader(http.StatusCreated)
 	} else {
+		log.Warn("74535::Could not commit link 400")
 		w.WriteHeader(http.StatusBadRequest)
 	}
 }
@@ -53,35 +57,39 @@ func GetLinkHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	address := fmt.Sprintf("%s:%s", "127.0.0.1", "6379")
 
+	log.Debug("15460::Instantiating Redis Connection")
 	c, err := redis.Dial("tcp", address)
 	if err != nil {
-		fmt.Printf("Redis Dial Error %e", err)
+		log.Error(fmt.Sprintf("15461::Couldn't connect to Redis %e", err))
 		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Couldn't contact data store")
+		return
 	}
 	defer c.Close()
 
+	log.Debug(fmt.Sprintf("15462::Searching redis for %s:arman:%s", vars["scope"], vars["name"]))
 	l, err := urlshortener.GetLink(c, vars["scope"], "arman", vars["name"])
 
 	if err != nil {
+		log.Error(fmt.Sprintf("15463::Couldn't retrieve value from Redis %e", err))
 		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Received an error while retrieving value from Redis")
-	}
-
-	// res, err := json.Marshal(l)
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Could not unmarshal link internally")
+		fmt.Fprintf(w, "Received an error while retrieving value from data store")
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%v", l)
+	log.Debug(fmt.Sprintf("15464::Received link, updating visit count"))
+	l.Visit(c)
+	log.Info(fmt.Sprintf("15465::Sending 301 %#v", l))
+	http.Redirect(w, r, l.Link, 301)
 }
 
 func main() {
+	Formatter := new(log.TextFormatter)
+	Formatter.FullTimestamp = true
+	log.SetFormatter(Formatter)
+	log.SetLevel(log.DebugLevel)
 	r := mux.NewRouter()
 	r.HandleFunc("/link/{scope}/{name}", GetLinkHandler).Methods("GET")
-	r.HandleFunc("/link/{name}", PostLinkHandler).Methods("POST")
+	r.HandleFunc("/link", PostLinkHandler).Methods("POST")
 	http.ListenAndServe(":3001", r)
 }

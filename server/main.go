ce65f4bcf7dac6b80d258ddb34307aad26a56497
Arman Elahi <arasela95@gmail.com>
2018-05-14 22:49:10 -0700

    Added support for default global lik

--- server/main.go
+++ server/main.go
@@ -29,7 +29,7 @@ func PostLinkHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	if l.Scope == "" {
-	l.Scope = "global"
+		l.Scope = "global"
 	}
 
 	l.User = "arman"
@@ -86,6 +86,37 @@ func GetLinkHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, l.Link, 301)
 }
 
+// GetGlobalLinkHandler retrieves data about the given link from the global scope
+func GetGlobalLinkHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	address := fmt.Sprintf("%s:%s", "127.0.0.1", "6379")
+
+	log.Debug("53170::Instantiating Redis Connection")
+	c, err := redis.Dial("tcp", address)
+	if err != nil {
+		log.Error(fmt.Sprintf("53171::Couldn't connect to Redis %e", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Couldn't contact data store")
+		return
+	}
+	defer c.Close()
+
+	log.Debug(fmt.Sprintf("53172::Searching redis for global:arman:%s", vars["name"]))
+	l, err := urlshortener.GetLink(c, "global", "arman", vars["name"])
+
+	if err != nil {
+		log.Error(fmt.Sprintf("53173::Couldn't retrieve value from Redis %e", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Received an error while retrieving value from data store")
+		return
+	}
+
+	log.Debug(fmt.Sprintf("53174::Received link, updating visit count"))
+	l.Visit(c)
+	log.Info(fmt.Sprintf("53175::Sending 301 %#v", l))
+	http.Redirect(w, r, l.Link, 301)
+}
+
 func main() {
 	Formatter := new(log.TextFormatter)
 	Formatter.FullTimestamp = true
@@ -93,6 +124,7 @@ func main() {
 	log.SetLevel(log.DebugLevel)
 	r := mux.NewRouter()
 	r.HandleFunc("/link/{scope}/{name}", GetLinkHandler).Methods("GET")
+	r.HandleFunc("/link/{name}", GetGlobalLinkHandler).Methods("GET")
 	r.HandleFunc("/link", PostLinkHandler).Methods("POST")
 	http.ListenAndServe(":3001", r)
 }
